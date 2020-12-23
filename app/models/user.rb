2879040310f96190740bda169609f0e4cab0e925
hardyhoshi <hardyhoshi@gmail.com>
2020-12-23 16:44:14 +0900

    deviseに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :birthday
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-      validates :last_name
-      validates :first_name
-    end
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-  end
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, format: { with: PASSWORD_REGEX }
-
+  
 end
