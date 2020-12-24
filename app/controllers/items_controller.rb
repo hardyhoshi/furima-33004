41d535630ea0ae301d76fb26bd3f549c0450e710
hardyhoshi <hardyhoshi@gmail.com>
2020-12-24 19:04:21 +0900

    画像の保存を許可するストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
