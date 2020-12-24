a5ffe6b6e90a747e4b38c8ea65cc56d78c753a85
hardyhoshi <hardyhoshi@gmail.com>
2020-12-24 19:11:07 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :price
+    validates :image
+  end
 end
