f541fb69e569244d4ba75b013038c572f647db27
hardyhoshi <hardyhoshi@gmail.com>
2020-12-25 12:44:40 +0900

    残りの４つのモデルでもActiveHashを使えるよう記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_charge
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :presence
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_day
 
   with_options presence: true do
     validates :name
@@ -17,5 +25,11 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  validates :item_status_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 end
