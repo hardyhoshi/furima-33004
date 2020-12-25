5fca754e8a09721914cf136755593466dcbd0566
hardyhoshi <hardyhoshi@gmail.com>
2020-12-25 12:20:21 +0900

    item_statusモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
   with_options presence: true do
     validates :name
     validates :info
