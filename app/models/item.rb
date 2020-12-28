81326814c810f993b218a8431cd6a3ef0886aba8
hardyhoshi <hardyhoshi@gmail.com>
2020-12-28 12:45:06 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,14 +14,14 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
 
   with_options presence: true do
-    validates :name, length: { maximum: 80 }
-    validates :info, length: { maximum: 2000 }
+    validates :name, length: { maximum: 40 }
+    validates :info, length: { maximum: 1000 }
     validates :category_id
     validates :item_status_id
     validates :shipping_charge_id
     validates :prefecture_id
     validates :delivery_day_id
-    validates :price, length: { minimum: 3, maximum: 9999999 }
+    validates :price, numericality: { greater_than: 300, less_than: 10000000 }
     validates :image
   end
 
