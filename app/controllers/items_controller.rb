3cb46d3b167acf11089838fbe6463b9c7006af2d
hardyhoshi <hardyhoshi@gmail.com>
2020-12-29 16:47:32 +0900

    繰り返し処理の@item = Item.find(params[:id])をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -54,6 +52,10 @@ class ItemsController < ApplicationController
       if (user_signed_in? && current_user.id != @item.user_id)
         redirect_to root_path
       end
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
 
   end
