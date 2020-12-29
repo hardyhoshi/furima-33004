26265beb3326d979d7d87aab935181041bfce6ea
hardyhoshi <hardyhoshi@gmail.com>
2020-12-29 16:39:35 +0900

    destroyアクション内の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -37,18 +37,23 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
-  def move_to_index
-    @item =Item.find(params[:id])
-    if (user_signed_in? && current_user.id != @item.user_id)
-      redirect_to root_path
-    end
-  end
-
+  
   private
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id,
-                                 :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+      :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+    end
+
+    def move_to_index
+      @item =Item.find(params[:id])
+      if (user_signed_in? && current_user.id != @item.user_id)
+        redirect_to root_path
+      end
+    end
+
   end
-end
