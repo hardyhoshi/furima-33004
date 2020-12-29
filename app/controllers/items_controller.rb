09db8c05f09afb9d482e4d171eb5855758a7e107
hardyhoshi <hardyhoshi@gmail.com>
2020-12-29 16:43:09 +0900

    move_to_indexにdestroyアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
