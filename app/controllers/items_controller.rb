e837971288589b5234ea3f01b4b6dc13ae2e53c5
hardyhoshi <hardyhoshi@gmail.com>
2021-01-04 12:30:25 +0900

    user_signed_in?の記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,20 +34,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  def move_to_index
-    @item = Item.find(params[:id])
-    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
-  end
-
+  
   private
-
+  
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id,
-                                 :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+      :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+    
+  def move_to_index
+      @item = Item.find(params[:id])
+      redirect_to root_path if current_user.id != @item.user_id
   end
 
   def set_item
-    @item = Item.find(params[:id])
+      @item = Item.find(params[:id])
   end
-  
+    
 end
