b3bdafc78c43100890ea20e16a9076eda8c6c6da
hardyhoshi <hardyhoshi@gmail.com>
2021-01-04 12:11:34 +0900

    rubocopでの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,13 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item =Item.find(params[:id])
-    if (user_signed_in? && current_user.id != @item.user_id)
-      redirect_to root_path
-    end
+    @item = Item.find(params[:id])
+    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
   end
 
   private
+
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id,
                                  :delivery_day_id, :price, :image).merge(user_id: current_user.id)
