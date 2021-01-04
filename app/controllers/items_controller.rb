24fbdc4f773d96560e9a2b68dde091482785bef6
hardyhoshi <hardyhoshi@gmail.com>
2021-01-04 12:28:29 +0900

    @item = Item.find(params[:id]) をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -47,4 +45,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id,
                                  :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
 end
