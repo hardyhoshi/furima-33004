dd282663a7f6fe9370513c422559d139979f47da
hardyhoshi <hardyhoshi@gmail.com>
2021-01-07 11:21:50 +0900

    保存処理の記述変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,6 +14,6 @@ class OrderAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, street_number: street_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, street_number: street_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
