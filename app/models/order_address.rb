9af53089ded2c2b363ba9cae7fd075f2573d91e7
hardyhoshi <hardyhoshi@gmail.com>
2021-01-07 11:19:51 +0900

    バリデーションの変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,11 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :street_number
-    validates :phone_number, numericality: {less_than_or_equal_to: 11}
-    validates :user_id
-    validates :item_id
+    validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :token
   end
 
   def save
