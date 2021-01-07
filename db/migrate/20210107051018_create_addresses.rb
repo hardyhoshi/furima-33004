4f4a18324453383ec8b6555faa9ab7cfaed91cd7
hardyhoshi <hardyhoshi@gmail.com>
2021-01-07 14:16:19 +0900

    データベース作り直し

--- db/migrate/20210107051018_create_addresses.rb
+++ db/migrate/20210107051018_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :municipality,   null: false
       t.string     :street_number,  null: false
       t.string     :building_name
-      t.integer    :phone_number,   null: false
+      t.string     :phone_number,   null: false
       t.references :order, foreign_key: true
       t.timestamps
     end
