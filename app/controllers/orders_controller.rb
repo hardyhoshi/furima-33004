68be37f48500b5882c0503b3c46b48904aee831a
hardyhoshi <hardyhoshi@gmail.com>
2021-01-07 12:21:56 +0900

    購入ボタンを押しても商品情報を取得できるように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,29 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :move_to_index, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item =Item.find(params[:item_id])
-    @order_address = OrderAddress.new
+    @order = Order.new(order_params)
+  end
+  
+  def new
   end
   
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
+       @order_address.save
+       redirect_to root_path
     else
-      render :index
+      render action: :index
     end
   end
 
   private
 
   def order_params
-    params.require(:item_order).permit(:postal_code, :prefecture, :municipality, :street_number, :building_name, :phone_number)
+    params.permit(:postal_code, :prefecture_id, :municipality, :street_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
   end
 
   def move_to_index
@@ -28,5 +31,9 @@ class OrdersController < ApplicationController
     if (current_user.id == @item.user_id)
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
