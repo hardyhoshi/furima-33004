779283ac433eec65bbfe2acfaa20f52695ba8c0e
hardyhoshi <hardyhoshi@gmail.com>
2021-01-11 11:38:21 +0900

    move_to_indexメソッド内に記述している@item = Item.find(params[:item_id])を省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -48,7 +48,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
   end
 
