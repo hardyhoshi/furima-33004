09057105b0cea027b5e68a29c684dfd0c396def6
hardyhoshi <hardyhoshi@gmail.com>
2021-01-13 17:59:39 +0900

    エラー文が少なく表示されるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, allow_blank: true, message: 'は英字と数字の両方を含めて設定してください' }
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: 'は全角(漢字・ひらがな・カタカナ)で入力してください' } do
       validates :last_name
       validates :first_name
     end
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: 'は全角で入力してください' } do
       validates :last_name_kana
       validates :first_name_kana
     end
     validates :birthday
   end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   has_many :items
   has_many :orders
