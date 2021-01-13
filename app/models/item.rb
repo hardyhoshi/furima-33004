f50e39c579c834e233f34496918d0c6ad7091269
hardyhoshi <hardyhoshi@gmail.com>
2021-01-13 18:33:48 +0900

    バリデーション処理をまとめた

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,22 +11,17 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
 
   with_options presence: true do
+    validates :image
     validates :name, length: { maximum: 40 }
     validates :info, length: { maximum: 1000 }
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_charge_id
-    validates :prefecture_id
-    validates :delivery_day_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 }
-    validates :image
+    with_options numericality: { other_than: 1, message: 'を選択してください' } do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :delivery_day_id
+    end
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000, allow_blank: true, message: 'は半角数字で300~9999999の間で入力してください'}
   end
 
-  with_options numericality: { other_than: 1, message: 'を選択してください' } do
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_charge_id
-    validates :prefecture_id
-    validates :delivery_day_id
-  end
 end
